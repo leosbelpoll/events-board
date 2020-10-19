88f5356943fb20a1724c58a93bf599b076c78bf4
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-10-19 20:50:29 -0300

    Frontend: Add redux to manage the app state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Provider } from "react-redux";
+
+import store from "redux/store";
 
 function App() {
-    return <div>App</div>;
+    return (
+        <Provider store={store}>
+            <div>App</div>
+        </Provider>
+    );
 }
 
 export default App;

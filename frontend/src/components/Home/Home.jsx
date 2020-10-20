7171403d8196d2c49e504d4b20c9da119e9bde5e
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-10-19 23:16:24 -0300

    Frontend: add Home components with event list and add some styles

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
 
+import { getNextDate } from "utils/date";
+import "./Home.scss";
+
 const Home = ({ eventsState, getEvents }) => {
     const { events, loading, error } = eventsState;
 
@@ -16,10 +19,50 @@ const Home = ({ eventsState, getEvents }) => {
     }
 
     return (
-        <>
-            <h1>Events board</h1>
-            {events && events.map((event) => <p>{event.title}</p>)}
-        </>
+        <div className="container">
+            <div className="row">
+                {events
+                    ? events.map((event) => {
+                          const nextDate = getNextDate(
+                              event.dates,
+                              "MMM do, yyyy @ HH:MM"
+                          );
+
+                          if (nextDate) {
+                              return (
+                                  <div className="col-md-4" key={event.id}>
+                                      <div className="card">
+                                          <img
+                                              src={event.eventImage}
+                                              className="card-img-top"
+                                              alt={event.title}
+                                              style={{
+                                                  height: "14rem",
+                                                  objectFit: "cover",
+                                              }}
+                                          />
+                                          <div className="card-body">
+                                              <h6 className="card-date">
+                                                  {nextDate}
+                                              </h6>
+                                              <h5 className="card-title">
+                                                  {event.title}
+                                              </h5>
+                                              <a
+                                                  href="#"
+                                                  className="btn btn-primary"
+                                              >
+                                                  View
+                                              </a>
+                                          </div>
+                                      </div>
+                                  </div>
+                              );
+                          }
+                      })
+                    : "No events to show"}
+            </div>
+        </div>
     );
 };
 
